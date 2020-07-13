f6043d52a388f32e2527406bf28f23ade7099eee
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-13 17:22:00 +0400

    Added Demo DB & Added GET Root

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "John",
+            email: "john@gmail.com",
+            password: "cookies",
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: "124",
+            name: "Sally",
+            email: "sally@gmail.com",
+            password: "bananas",
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+};
+
+app.get("/", (req, res)=> {
+    res.send("this is working");
+});
+
 app.listen(PORT, () => {
     console.log("app is running on port " + PORT);
 });
