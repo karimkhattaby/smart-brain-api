492259de2d0b4fb87bc7ff05f0aed1865b499409
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-15 13:07:08 +0400

    Adding log for Debugging

--- controllers/register.js
+++ controllers/register.js
@@ -25,7 +25,10 @@ const handleRegister = (req, res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback);
     })
-    .catch(err => res.status(400).json("Unable to Register"));
+    .catch(err => {
+        console.log(err);
+        res.status(400).json("Unable to Register");
+    });
 };
 
 module.exports = {
